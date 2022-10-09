643c796a4ff5a24c537c947e78cc629566356332
Codi <codi798@gmail.com>
2022-10-09 11:31:38 -0300

    Starting to implement Board and Piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,10 +5,27 @@ public class Board {
 	private int rows;
 	private int columms;
 	private Piece[][] pieces;
+	
 	public Board(int rows, int columms, Piece[][] pieces) {
 		this.rows = rows;
 		this.columms = columms;
 		pieces = new Piece[rows][columms];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumms() {
+		return columms;
+	}
+
+	public void setColumms(int columms) {
+		this.columms = columms;
 	}
 	
 
